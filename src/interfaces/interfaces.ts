3129a32f893b1d43a207f7739aa92017c287fbea
Monish <Monishatom@outlook.com>
2022-10-26 15:21:50 +0530

    refactor: change some code

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -35,46 +35,23 @@ export interface ImageDatas {
 }
 
 export interface UserDatas {
-  id: number;
-  width: number;
-  height: number;
-  urls: {
-    raw: string;
-    regular: string;
-    small: string;
-  };
-  links: {
-    self: string;
-    download: string;
-  };
-  user: {
-    id: string;
-    username: string;
-    name: string;
-    bio: string;
-    first_name: string;
-    last_name: string;
-    location: string;
-    profile_image: {
-      small: string;
-      medium: string;
-      large: string;
-    };
-    total_likes: number;
-    total_photos: number;
-    social: {
-      instagram_username: string;
-      portfolio_url: string;
-      twitter_username: string;
-    };
-  };
-}
-
-export interface Photos {
   id: string;
   username: string;
   name: string;
   bio: string;
+  location: string;
+  profile_image: {
+    small: string;
+    medium: string;
+    large: string;
+  };
+  total_likes: number;
+  total_photos: number;
+  social: {
+    instagram_username: string;
+    portfolio_url: string;
+    twitter_username: string;
+  };
   photos: [
     {
       id: string;
@@ -85,4 +62,27 @@ export interface Photos {
       };
     }
   ];
+  badge: {
+    title: string;
+    primary: boolean;
+    slug: string;
+  };
+  followers_count: number;
+  following_count: number;
+  downloads: number;
+}
+
+export interface UserPhotos {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  color: string;
+  width: number;
+  height: number;
+  urls: {
+    raw: string;
+    regular: string;
+    small: string;
+  };
+  likes: number;
 }
